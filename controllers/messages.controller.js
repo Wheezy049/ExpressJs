cdcb07e5521de84d4dac0f3afd23282209113019
Dev_Faruq <olatoyesefaruq@gmail.com>
2025-03-15 15:25:42 +0100

    Template engines: Handlebars(hbs)

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,12 @@
+const path = require('path'); 
+
 function getMessages(req, res) {
-    res.send('<ul><li>Message 1</li><ul>');
+    // res.send('<ul><li>Message 1</li><ul>');
+    // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'image1.png'));
+    res.render('messages', {
+        title: 'Messages',
+        friend: 'Elon Musk'
+    });
 };
 
 function postMessage(req, res) {
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const friendsRouter = require('./routes/friends.router');
 const messagesRouter = require('./routes/messages.router');
 
 const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 const PORT = 3000;
 
@@ -14,8 +18,15 @@ app.use((req, res, next) => {
     console.log(`${req.method} request for ${req.baseUrl}${req.url} in ${delta}ms`);
 });
 
+app.use('/site', express.static(path.join(__dirname,'public')));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Express Project',
+        caption: 'This is a caption'
+    });
+});
 app.use('/friends', friendsRouter);
 app.use('/messages', messagesRouter)
 
